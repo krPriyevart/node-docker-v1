8d6481a445afaeea9f1eacc558afa2ff435cc25d
krpriyevart <kumarpriyevart@gmail.com>
2026-08-21 16:29:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT ?? 8080;
+
+app.get('/', (req,res)=>{
+    return res.json({msg: 'Helloooo sir🙂 v1'})
+});
+
+app.get('/health', (req,res)=>{
+    return res.json({msg: 'ok'});
+});
+
+app.listen(PORT,()=>{
+    console.log('Server is running on port:',PORT);
+})
